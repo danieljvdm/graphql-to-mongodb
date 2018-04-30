d2e40ae76589e1f29320537291c7d80955c2024d
Daniel van der Merwe <danieljmerwe@gmail.com>
2018-04-29 21:07:35 -0400

    Various fixes

--- src/queryResolver.ts
+++ src/queryResolver.ts
@@ -1,53 +1,82 @@
-import getMongoDbFilter from './mongoDbFilter';
-import getMongoDbProjection from './mongoDbProjection';
-import { getGraphQLFilterType } from './graphQLFilterType';
-import getGraphQLSortType from './graphQLSortType';
-import GraphQLPaginationType from './graphQLPaginationType';
-import { isType, GraphQLResolveInfo, GraphQLFieldResolver, GraphQLObjectType } from 'graphql';
-import { FICTIVE_SORT, clear } from './common';
+import getMongoDbFilter from "./mongoDbFilter";
+import getMongoDbProjection from "./mongoDbProjection";
+import { getGraphQLFilterType } from "./graphQLFilterType";
+import getGraphQLSortType from "./graphQLSortType";
+import GraphQLPaginationType from "./graphQLPaginationType";
+import {
+  isType,
+  GraphQLResolveInfo,
+  GraphQLFieldResolver,
+  GraphQLObjectType,
+  GraphQLFieldConfigArgumentMap
+} from "graphql";
+import { FICTIVE_SORT, clear } from "./common";
 
 export interface QueryCallback<TSource, TContext> {
-    (
-        filter: object,
-        projection: object,
-        options: object,
-        source: TSource,
-        args: { [argName: string]: any },
-        context: TContext,
-        info: GraphQLResolveInfo
-    ): Promise<any>
+  (
+    filter: { [key: string]: any },
+    projection: { [key: string]: any },
+    options: { [key: string]: any },
+    source: TSource,
+    args: { [argName: string]: any },
+    context: TContext,
+    info: GraphQLResolveInfo
+  ): Promise<any>;
 }
 
 export interface QueryOptions {
-  differentOutputType: boolean
+  differentOutputType: boolean;
 }
 
 const defaultOptions: QueryOptions = {
   differentOutputType: false
 };
 
-export function getMongoDbQueryResolver<TSource, TContext>(graphQLType: GraphQLObjectType, queryCallback: QueryCallback<TSource, TContext>, queryOptions: QueryOptions = defaultOptions)
-    : GraphQLFieldResolver<TSource, TContext> {
-    if (!isType(graphQLType)) throw 'getMongoDbQueryResolver must recieve a graphql type'
-    if (typeof queryCallback !== 'function') throw 'getMongoDbQueryResolver must recieve a queryCallback function'
+export function getMongoDbQueryResolver<TSource, TContext>(
+  graphQLType: GraphQLObjectType,
+  queryCallback: QueryCallback<TSource, TContext>,
+  queryOptions: QueryOptions = defaultOptions
+): GraphQLFieldResolver<TSource, TContext> {
+  if (!isType(graphQLType))
+    throw "getMongoDbQueryResolver must recieve a graphql type";
+  if (typeof queryCallback !== "function")
+    throw "getMongoDbQueryResolver must recieve a queryCallback function";
 
-    return async (source: TSource, args: { [argName: string]: any }, context: TContext, info: GraphQLResolveInfo): Promise<any> => {
-        const filter = getMongoDbFilter(graphQLType, args.filter);
-        let projection;
-        if(!queryOptions.differentOutputType) projection = getMongoDbProjection(info.fieldNodes, graphQLType);
-        const options: { sort?: object, limit?: number, skip?: number } = {};
-        if (args.sort) options.sort = clear(args.sort, FICTIVE_SORT);
-        if (args.pagination && args.pagination.limit) options.limit = args.pagination.limit;
-        if (args.pagination && args.pagination.skip) options.skip = args.pagination.skip;
+  return async (
+    source: TSource,
+    args: { [argName: string]: any },
+    context: TContext,
+    info: GraphQLResolveInfo
+  ): Promise<any> => {
+    const filter = getMongoDbFilter(graphQLType, args.filter);
+    let projection;
+    if (!queryOptions.differentOutputType)
+      projection = getMongoDbProjection(info.fieldNodes, graphQLType);
+    const options: { sort?: object; limit?: number; skip?: number } = {};
+    if (args.sort) options.sort = clear(args.sort, FICTIVE_SORT);
+    if (args.pagination && args.pagination.limit)
+      options.limit = args.pagination.limit;
+    if (args.pagination && args.pagination.skip)
+      options.skip = args.pagination.skip;
 
-        return await queryCallback(filter, projection, options, source, args, context, info);
-    }
+    return await queryCallback(
+      filter,
+      projection,
+      options,
+      source,
+      args,
+      context,
+      info
+    );
+  };
 }
 
-export function getGraphQLQueryArgs(graphQLType: GraphQLObjectType): object {
-    return {
-        filter: { type: getGraphQLFilterType(graphQLType) },
-        sort: { type: getGraphQLSortType(graphQLType) },
-        pagination: { type: GraphQLPaginationType }
-    };
+export function getGraphQLQueryArgs(
+  graphQLType: GraphQLObjectType
+): GraphQLFieldConfigArgumentMap {
+  return {
+    filter: { type: getGraphQLFilterType(graphQLType) },
+    sort: { type: getGraphQLSortType(graphQLType) },
+    pagination: { type: GraphQLPaginationType }
+  };
 }
